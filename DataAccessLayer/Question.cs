ed97a856e9b19a0d082d30d56a9014cff37db6a2
Deathype <camille.galonzka@gmail.com>
2019-12-17 16:55:39 +0100

    maj

---
+++ DataAccessLayer/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    class Question
+    {
+        public int id { get; set; }
+        public String Theme { get; set; }
+        public String Nom { get; set; }
+    }
+}
---
+++ DataAccessLayer/Repondre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    class Repondre
+    {
+        public Stagiaire stagiaire { get; set; }
+        public Question question { get; set; }
+        public Reponses reponses { get; set; }
+        public String repStagiaire { get; set; }
+    }
+}
