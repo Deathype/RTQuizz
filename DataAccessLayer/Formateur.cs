aa1cd8b9e54984d9dd6cfd052376b54c9ead0311
SushyTheFish <clement1.coelho@gmail.com>
2019-12-17 16:52:50 +0100

    MAJ Classe

---
+++ DataAccessLayer/Formateur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    class Formateur
+    {
+        public int IdFormateur { get; set; }
+
+        public String Nom { get; set; }
+
+        public String Prenom { get; set; }
+
+        public int ProfMatiere { get; set; }
+
+    }
+}
---
+++ DataAccessLayer/Stagiaire.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    class Stagiaire
+    {
+        public int IdStagiaire { get; set; }
+        public String Nom { get; set; }
+
+        public String Prenom { get; set; }
+    }
+}
