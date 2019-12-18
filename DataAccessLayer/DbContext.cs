996b76e93ceee32e92475209094e96d81bdce4bf
SushyTheFish <clement1.coelho@gmail.com>
2019-12-18 11:46:12 +0100

    Maj DBContext

--- DataAccessLayer/DbContext.cs
+++ DataAccessLayer/DbContext.cs
@@ -21,6 +21,12 @@ namespace DataAccessLayer
         public DbSet<Stagiaire> Stagiaires { get; set; }
 
           public DbSet<Classe> Classes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Repondre>()
+                .HasKey(r => new { r.IdStagiare, r.IdQuestion, r.IdReponse });
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=QuizDB;Trusted_Connection=True;");
