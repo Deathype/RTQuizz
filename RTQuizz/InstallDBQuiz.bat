5a5ad6070098061d2880e29fed4d0195ea6f13ea
unknown <marouen1931@hotmail.com>
2019-12-18 11:52:29 +0100

    Insert Script

--- RTQuizz/InstallDBQuiz.bat
+++ RTQuizz/InstallDBQuiz.bat
@@ -1,4 +1,4 @@
 sqlcmd -S (localdb)\MSSQLLocalDB -d master -i ScriptDB.sql -E
 
-sqlcmd -S (localdb)\MSSQLLocalDB -d master -i ScriptInsert.sql -E
+sqlcmd -S (localdb)\MSSQLLocalDB -d QuizDB -i ScriptInsert.sql -E
 pause
